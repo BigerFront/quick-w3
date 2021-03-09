c4608f94c30bcb75fd07c4fbfaaf0b726fc05793
developer <team-developer@gmail.com>
2021-03-09 20:37:16 +0800

    init w3 project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  endOfLine: "auto"
+};
